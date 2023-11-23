287812232d7ff95659a7aa014242473748384c5f
azulena <101emeritems250723@gmail.com>
2023-11-23 18:40:20 -0500

    se colo en nombre del usuario y cierre de secion y en inicio se puse responsive falta el resto

--- explotour.js
+++ explotour.js
@@ -26,39 +26,44 @@ function guardarUsuario() {
     localStorage.setItem('nombreUsuario', nombre);
 
     // Redirige al usuario a la página de inicio de sesión
-    window.location.href = 'explotour.html';
+    window.location.href = 'inicioexplo.html';
 }
 
 // Función para iniciar sesión
 function iniciarSesion() {
-    // Captura el correo y la contraseña ingresados
+    
     var correo = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Recupera los datos del usuario registrado del localStorage
+    
     var usuarioJSON = localStorage.getItem('usuario');
 
     if (usuarioJSON) {
-        // Convierte los datos del usuario almacenados en localStorage a un objeto
+     
         var usuario = JSON.parse(usuarioJSON);
 
-        // Comprueba si el correo y la contraseña coinciden con los datos almacenados
+        
         if (correo === usuario.correo && password === usuario.contraseña) {
-            // Los datos coinciden, redirige al usuario a la página de inicio
+            
             window.location.href = 'explotour.html';
         } else {
-            // Los datos no coinciden, muestra un mensaje de error
+            
             alert('Cuenta no registrada o datos incorrectos.');
         }
     } else {
-        // No se encontró ningún usuario registrado, muestra un mensaje de error
+        
         alert('Cuenta no registrada o datos incorrectos.');
     }
 }
 
-// Verifica si ya hay un nombre de usuario en localStorage y actualiza la barra de navegación
-const nombreUsuario = localStorage.getItem('nombreUsuario');
-if (nombreUsuario) {
-    const perfilLink = document.querySelector(".enlace-perfil");
-    perfilLink.textContent = `PERFIL (${nombreUsuario})`;
-}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const nombreUsuario = localStorage.getItem('nombreUsuario');
+    if (nombreUsuario) {
+        const saludoUsuarioElement = document.getElementById('saludo-usuario');
+        if (saludoUsuarioElement) {
+            saludoUsuarioElement.textContent = `¡Bienvenido, ${nombreUsuario}!`;
+        }
+    }
+});
